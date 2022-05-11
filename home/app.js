6efcc8f10fb419a2a5a0a90eb71d1cd3eb2ad561
Brian <briantkho@gmail.com>
2022-05-10 23:33:03 -0700

    Vaccine data done

--- home/app.js
+++ home/app.js
@@ -34,10 +34,10 @@ dropdownProvinces();
 
 // Fetch and Display Data of All of Canada
 // !!! Todo: change summary data to reports data
-let caseNumber = document.querySelector(".newCases");
-let testNumber = document.querySelector(".newTests");
-let recoveriesNumber = document.querySelector(".newRecoveries");
-let headerName = document.querySelector(".header");
+const caseNumber = document.querySelector(".newCases");
+const testNumber = document.querySelector(".newTests");
+const recoveriesNumber = document.querySelector(".newRecoveries");
+const headerName = document.querySelector(".header");
 
 const canadaSummary = async () => {
   const summaryEndpoint = "/summary";
@@ -107,7 +107,7 @@ dropdown.onchange = () => {
 };
 
 // Last Updated
-let update = document.querySelector(".updated");
+const summaryUpdate = document.querySelector(".summaryUpdated");
 const lastUpdated = async () => {
   const updateEndpoint = "/summary/split";
   const urlToFetch = `${covidUrl}${updateEndpoint}`;
@@ -122,7 +122,7 @@ const lastUpdated = async () => {
     if (response.ok) {
       const jsonResponse = await response.json();
       const updates = jsonResponse.last_updated;
-      update.innerHTML = `Last Updated: ${updates}`;
+      summaryUpdate.innerHTML = `Last Updated: ${updates}`;
     }
   } catch (error) {
     alert(error);
@@ -133,6 +133,15 @@ lastUpdated();
 // !!! Display Vaccination Info
 
 // Vaccinations Data in Canada
+const htmlChangeVaccinations = document.querySelector(".numNewVaccinations");
+const htmlChangeVaccinated = document.querySelector(".numNewVaccinated");
+const htmlChangeBoosters = document.querySelector(".numNewBoosters");
+const htmlChangeDistributed = document.querySelector(".numNewDistributed");
+const htmlTotalVaccination = document.querySelector(".numTotalVaccination");
+const htmlTotalVaccinated = document.querySelector(".numTotalVaccinated");
+const htmlTotalBoosters = document.querySelector(".numTotalBoosters");
+const htmlTotalDistributed = document.querySelector(".numTotalDistributed");
+const htmlVaccinateUpdated = document.querySelector(".vaccineUpdated");
 
 const canadaVaccineData = async () => {
   const vaccineCanadaEndpoint = "/summary";
@@ -142,18 +151,37 @@ const canadaVaccineData = async () => {
     const response = await fetch(urlToFetch, {
       method: "GET",
       headers: {
-        accept: application / json,
+        accept: "application/json",
       },
     });
     if (response.ok) {
       const jsonResponse = await response.json();
-      const newVaccination = "";
-      const newVaccinated = "";
-      const newBoosters = "";
-      const newDistributed = "";
-      //!!! Fill out above with jsonResoponse...
+      const newVaccination = jsonResponse.data[0].change_vaccinations;
+      const newVaccinated = jsonResponse.data[0].change_vaccinated;
+      const newBoosters =
+        Number(jsonResponse.data[0].change_boosters_1) +
+        Number(jsonResponse.data[0].change_boosters_2);
+      const newDistributed = jsonResponse.data[0].change_vaccines_distributed;
+      const totalVaccination = jsonResponse.data[0].total_vaccinations;
+      const totalVaccinated = jsonResponse.data[0].total_vaccinated;
+      const totalBoosters =
+        Number(jsonResponse.data[0].total_boosters_1) +
+        Number(jsonResponse.data[0].total_boosters_2);
+      const totalDistributed = jsonResponse.data[0].total_vaccines_distributed;
+      const vaccineUpdated = jsonResponse.last_updated;
+
+      htmlChangeVaccinations.innerHTML = newVaccination;
+      htmlChangeVaccinated.innerHTML = newVaccinated;
+      htmlChangeBoosters.innerHTML = newBoosters;
+      htmlChangeDistributed.innerHTML = newDistributed;
+      htmlTotalVaccination.innerHTML = totalVaccination;
+      htmlTotalVaccinated.innerHTML = totalVaccinated;
+      htmlTotalBoosters.innerHTML = totalBoosters;
+      htmlTotalDistributed.innerHTML = totalDistributed;
+      htmlVaccinateUpdated.innerHTML = `Last Updated: ${vaccineUpdated}`;
     }
   } catch (error) {
     alert(error);
   }
 };
+canadaVaccineData();
