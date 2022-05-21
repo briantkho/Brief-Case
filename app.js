5bd1f7d26096d0bdcad3f2c7d2e8ad2b8671d5d1
Brian <briantkho@gmail.com>
2022-05-21 14:34:13 -0700

    navbar links with animation

--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 
 const covidUrl = "https://api.covid19tracker.ca";
 
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
+  anchor.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute("href")).scrollIntoView({
+      behavior: "smooth",
+    });
+  });
+});
+
 // Vaccination HTML
 const htmlChangeVaccinations = document.querySelector(".numNewVaccinations");
 const htmlChangeVaccinated = document.querySelector(".numNewVaccinated");
